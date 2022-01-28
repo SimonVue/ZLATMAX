202128062e7b76798876ead1997ef0e2fc43c083
SimonVue <viktorov_@mail.ru>
2022-01-28 14:37:12 +0300

    add script.js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,4 +3,30 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+  const targetElement = e.target;
+  if (targetElement.closest('[data-parent]')) {
+    const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
+    const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
+    const catalogMenu = document.querySelector('.catalog-header');
+
+    if (subMenu) {
+      const activeLink = document.querySelector('._sub-menu-active');
+      const activeBlock = document.querySelector('._sub-menu-open');
+
+      if (activeLink && activeLink !== targetElement) {
+        activeBlock.classList.remove('_sub-menu-open');
+        activeLink.classList.remove('_sub-menu-active');
+      }
+      targetElement.classList.toggle('_sub-menu-active');
+      subMenu.classList.toggle('_sub-menu-open');
+    } else {
+      console.log('Ой, ОЙ, нет такого меню');
+    }
+    e.preventDefault();
+  }
+}
+
 
